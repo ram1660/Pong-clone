2fe36da32b66c98a6b9fb725b12f3f3c3cffdecd
Ram Torgeman <ramtorgeman@gmail.com>
2022-12-03 12:30:15 +0200

    Defined colors improved arguments for cleaner code.

--- main.py
+++ main.py
@@ -9,14 +9,17 @@ from gameObjects.Entity import Entity
 
 size = width, height = 1280, 720
 
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0,)
+
 
 def init_game_objects(screen: pygame.Surface) -> list[Entity]:
     game_objects = []
-    paddle_width, paddle_height = 10, 30
-    game_objects.append(Player('Player 1', x=paddle_width, y=height // 2 - paddle_height, width=paddle_width))
-    game_objects.append(Enemy(x=width - paddle_width, y=height // 2 - paddle_height, height=paddle_height, width=paddle_width))
-    game_objects.append(Divider(width // 2, 0))
-    game_objects.append(Ball(width // 2, height // 2))
+    game_objects.append(Player('Player 1', screen, WHITE,
+                        y=height // 2))
+    game_objects.append(Enemy(screen, WHITE, x=width, y=height // 2))
+    game_objects.append(Divider(width // 2, 0, screen, WHITE))
+    game_objects.append(Ball(screen, WHITE, width // 2, height // 2))
     return game_objects
 
 
@@ -24,10 +27,13 @@ def render_ui() -> None:
     pass
 
 
+def check_collision() -> None:
+    pass
+
+
 def render_game_objects(game_objects: list[Entity]) -> None:
     for game_object in game_objects:
         game_object.render()
-    pygame.display.flip()
 
 
 def update_game_objects(game_objects) -> None:
@@ -45,6 +51,10 @@ def main() -> None:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+        screen.fill(BLACK)
+        render_game_objects(game_objects)
+        pygame.display.flip()
+        clock.tick(60)
 
 
 if __name__ == '__main__':
