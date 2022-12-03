3f59c3e2c7252ec189c698e7f45a629092448977
Ram Torgeman <ramtorgeman@gmail.com>
2022-12-03 16:29:13 +0200

    Fixed wrong functions implementations in enemy.

--- gameObjects/Enemy.py
+++ gameObjects/Enemy.py
@@ -3,12 +3,9 @@ import pygame
 class Enemy(Paddle):
     OBJECT_NAME = 'enemy'
     def __init__(self, surface: pygame.Surface, color: tuple, x=0, y=0, direction=1, speed=1):
-        super().__init__(surface, color, x, y, direction, speed)
+        super().__init__(surface, color, x - (super().PADDLE_WIDTH * 2), y, direction, speed)
     
-    def update(self, pygame):
-        pass
-    
-    def render(self) -> None:
+    def update(self):
         pass
     
     def __str__(self):
